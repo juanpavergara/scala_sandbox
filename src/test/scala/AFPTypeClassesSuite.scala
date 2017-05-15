8068dea98d1e454ab94f187b95470bcc71721aef
Juan Pablo Vergara <juanpablo@seven4n.com>
2017-05-15 14:57:51 -0500

    Mejora con sugerencia de operadores implícitos

--- src/test/scala/AFPTypeClassesSuite.scala
+++ src/test/scala/AFPTypeClassesSuite.scala
@@ -5,34 +5,46 @@ class AFPTypeClassesSuite extends FunSuite {
   test("Ejercicio type class"){
 
     case class ADT1(s: String, i:Int)
-
     case class ADT2(s: String, i:Int)
 
     trait Conversor[I, T]{
       def toADT(t:I):T
+//      def toADT: (I)=>T
     }
 
-    object Conversor{
-      implicit object ConversorADT1 extends Conversor[Tuple2[String,Int],ADT1]{
-        def toADT(t:(String, Int)) = ADT1(t._1, t._2)
+    trait ConversorSyntax{
+      implicit class ConversorOps[I](i:I){
+        def toADT[T](implicit c:Conversor[I,T]): T = c.toADT(i)
+      }
+    }
+
+    trait ConversorInstances extends ConversorSyntax{
+      implicit def ConversorADT1 = new Conversor[(String,Int),ADT1]{
+         def toADT(t:(String, Int)) = ADT1(t._1, t._2)
       }
 
-      implicit object ConversorADT2 extends Conversor[Tuple2[String,Int],ADT2]{
+      implicit def ConversorADT2 = new Conversor[(String,Int),ADT2]{
         def toADT(t:(String, Int)) = ADT2(t._1, t._2)
       }
+
     }
 
-    def toADT[I,T](t:I)(implicit c: Conversor[I,T]):T = {
-        c.toADT(t)
-    }
+    object conversores extends ConversorInstances
 
-    val adt1: ADT1 = toADT[Tuple2[String,Int], ADT1](("a", 1))
-    val adt2: ADT2 = toADT[Tuple2[String,Int], ADT2](("a", 1))
+    import conversores.ConversorADT1
+    import conversores.ConversorADT2
+    import conversores.ConversorOps
+    val adt1 = ("5", 5).toADT[ADT1]
+    val adt2 = ("5", 5).toADT[ADT2]
 
-    assert(adt1.s == "a")
-    assert(adt1.i == 1)
-    assert(adt2.s == "a")
-    assert(adt2.i == 1)
+    assert(adt1.s == "5")
+    assert(adt1.i == 5)
+
 
   }
+
+
+
+
+
 }
