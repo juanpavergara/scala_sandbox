91f996e0c30f6872e7621e95a15521faa56f46ca
Juan Pablo Vergara <juanpablo@seven4n.com>
2017-05-16 12:46:47 -0500

    Adicion de test de type class con sum types a la suite adecuada

--- src/test/scala/AFPTypeClassesSuite.scala
+++ src/test/scala/AFPTypeClassesSuite.scala
@@ -2,6 +2,76 @@ import org.scalatest.FunSuite
 
 class AFPTypeClassesSuite extends FunSuite {
 
+
+  test("Type Class design pattern"){
+
+    /*
+    Object para hacer disponible al trait interno el cual es el Type Class
+     */
+    object MyTypeClassObject{
+
+      /*
+      Este trait es el Type Class *per se*
+      Es parametrizado. La idea es que las funciones del trait estén parametrizadas a los tipos
+      a los cuales está parametrizado el trait.
+      Preferiblemente se debe evitar tener tipos concretos en las firmas de las funciones del Type Class
+       */
+      trait MyTypeClass[T]{
+        def myOp(i:T):T
+      }
+    }
+
+    import MyTypeClassObject._
+
+    /*
+    Ahora hacemos un *miembro* del Type Class
+    Aqui se materializa el polimorfismo ad-hoc pues estamos implementando myOp
+    específicamente para el tipo String (en este ejemplo).
+
+    Se hace implicit para que la conversión la haga el compilador y no se haga explícitamente.
+    Es posible que esto se vea más claro a la hora de usarlo.
+     */
+    implicit object MyTypeClassMemberForString extends MyTypeClass[String] {
+      def myOp(i:String) = s"Hello $i"
+    }
+
+    /*
+    Ahora definimos una funcion cualquiera foo en la cual es importante destacar lo siguiente:
+    1. Es parametrizada en T. Se busca que sea genérica via parametrizacion.
+    2. Depende de un MyTypeClass para internamente valerse de myOp para su comportamiento.
+    3. No depende cómo tc hace myOp. Solo depende de lo que promete myOp en el Type Class.
+     */
+
+    def foo[T](t:T)(implicit tc: MyTypeClass[T]) : T = {
+      tc.myOp(t)
+    }
+
+    /*
+    Se puede definir lo mismo pero con otra sintaxis como se hace a continuacion con la funcion bar
+    (hace exactamente lo mismo que foo). Importante tener en cuenta:
+
+    1. El tipo parametrizado es T:MyTypeClass y no T solito. Esto es una alternativa
+    que solo funciona para Type Classes de un solo tipo parametrizado.
+    2. Ya no hay segundo parametri implicit y se reemplaza por el implicitly en el cuerpo
+    de la funcion al evaluar myOp
+     */
+    def bar[T:MyTypeClass](t:T) : T = {
+      implicitly[MyTypeClass[T]].myOp(t)
+    }
+
+
+
+    /*
+    La siguiente evaluacion funciona porque existe un implicit object MyTypeClassMemberForString
+     */
+    assert(foo("Juancho") == "Hello Juancho")
+    assert(bar("Juancho") == "Hello Juancho")
+    /*
+    La siguiente evaluacion no compila porque no existe definicion implicita de un MyTypeClass[Int]
+     */
+    assertDoesNotCompile("foo(1) == \"Hello Juancho\"")
+  }
+
   test("Ejercicio type class"){
 
     case class ADT1(s: String, i:Int)
